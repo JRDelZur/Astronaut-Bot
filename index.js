35395e9b9076bce91eff5da45b51416284f056a3
JRDelZur <>
2021-05-27 01:57:25 +0000

    **Se agrego el modulo express para mantener vivo al bot 24/7

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at Port: ${port}`);
+});
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const { Client, MessageEmbed, Collection, Guild } = require("discord.js");
-const keepAlive = require('./server.js');
 const mySecret = process.env['token']
 
 const fs = require('fs');
